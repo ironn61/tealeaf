3b7a3337b2fc0b21679cab7bffafcb17be2e73f5
ironn61 <ironn61@gmail.com>
2016-03-08 17:30:26 +0800

    hw2

---
+++ basic2.rb
@@ -0,0 +1,5 @@
+i = 1234
+i / 1000   #puts 1
+i % 1000 / 100   #modulo% puts 234, then divide by 100 =2
+i % 1000 % 100 / 10 #modulo 1000 puts 234, then %100 puts 34 / 10 = 3
+i % 1000 % 100 % 10
---
+++ basic3.rb
@@ -0,0 +1,4 @@
+config = { :kingkong=>1975, :xxx=>2004, :big=>2013, :small=>2001, :superman=>1981 }
+config.each do |key, value|
+	puts "#{value}"
+end
